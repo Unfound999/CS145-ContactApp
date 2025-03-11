c89d680cf0cc2d8bc85d81c3479da6ec4005b653
Jackson Jenks <jacksocrod@gmail.com>
2025-03-10 17:10:22 -0700

    Main changes

--- src/ContactMain.java
+++ src/ContactMain.java
@@ -7,7 +7,6 @@ public class ContactMain {
     }
 
     public static void addContactMain(BinarySearchTree<Contact> Tree) {
-
         Scanner input = new Scanner(System.in);
 
         System.out.print("Please provide the first name of the contact:");
@@ -29,8 +28,9 @@ public class ContactMain {
         String address = input.nextLine();
 
         Contact Guy = new Contact(address, email, fName, lName, pNum, zip);
-
         Tree.add(Guy);
+
+        System.out.print("Contact Added!");
     }
 
     public static void viewContact(BinarySearchTree<Contact> Tree) {
@@ -63,10 +63,23 @@ public class ContactMain {
         String pNum = input.nextLine();
 
         Contact Guy = new Contact(fName, lName, pNum);
-        Guy = Tree.remove(Guy);
+        Tree.remove(Guy);
+        System.out.print("Contact Removed!");
     }
 
     public static void viewAllContacts(BinarySearchTree<Contact> Tree) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please provide the first name of the contact:");
+        String fName = input.nextLine();
+
+        System.out.print("Please provide the last name of the contact:");
+        String lName = input.nextLine();
+
+        System.out.print("Please provide the phone number of the contact:");
+        String pNum = input.nextLine();
+
+        Contact Guy = new Contact(fName, lName, pNum);
     }
 }
