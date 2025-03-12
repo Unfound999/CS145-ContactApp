eb560a7714c7d1d5bee82769b6acedddb7efa44d
Christopher Waschke <Chrisdwaschke@gmail.com>
2025-03-11 22:40:34 -0700

    Fixed typos, moved exceptions into their own files.

---
+++ src/NodeNotFoundException.java
@@ -0,0 +1,9 @@
+/*
+ * Authors: Christopher Waschke, Brody Weinkauf, Jackson Jenks
+ * Assignment: Week 9 - Binary Search Trees
+ * Description: Contact Application Using Binary Search Tree
+ */
+
+package src;
+
+public class NodeNotFoundException extends RuntimeException {}
